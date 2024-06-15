a1a2e6dfefaace68cd191733c4915186b6870483
jasmeen <jasmeenbano12@gmail.com>
2024-06-15 15:24:24 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header';
+import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
     
       <Header/>
+      <Banner/>
     </div>
   );
 }
